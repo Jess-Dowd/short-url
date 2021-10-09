ab1e123c1d0b212c1a008a208b203610afd42510
jess-dowd <jessalyndowd@gmail.com>
2021-10-09 21:53:49 +0700

    commit 2

---
+++ main.js
@@ -0,0 +1,58 @@
+var endpoint = "https://ef45989b94af8fb790427960f84d98ea.m.pipedream.net";
+
+function geturl(){
+    var url = document.getElementById("urlinput").value;
+    var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
+    if(!protocol_ok){
+        newurl = "http://"+url;
+        return newurl;
+        }else{
+            return url;
+        }
+}
+
+function getrandom() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
+function genhash(){
+    if (window.location.hash == ""){
+        window.location.hash = getrandom();
+    }
+}
+
+function send_request(url) {
+    this.url = url;
+    $.ajax({
+        'url': endpoint + "/" + window.location.hash.substr(1),
+        'type': 'POST',
+        'data': JSON.stringify(this.url),
+        'dataType': 'json',
+        'contentType': 'application/json; charset=utf-8',
+        'X-Master-Key': '$2b$10$awI5kl/9orunGmcpE2WBo.AjejhUK5Ml0nyHSKLm.tDWhwH30whUK'
+})
+}
+
+function shorturl(){
+    var longurl = geturl();
+    genhash();
+    send_request(longurl);
+}
+
+var hashh = window.location.hash.substr(1)
+
+if (window.location.hash != "") {
+    $.getJSON(endpoint + "/" + hashh, function (data) {
+        data = data["result"];
+
+        if (data != null) {
+            window.location.href = data;
+        }
+
+    });
+}
