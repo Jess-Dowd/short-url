31ff875f871829b1ac6715e61ef1b33cd71caa6a
jess-dowd <jessalyndowd@gmail.com>
2021-10-10 19:35:00 +0700

    8

--- main.js
+++ main.js
@@ -88,16 +88,16 @@ function shorturl() {
 var hashh = window.location.hash.substr(1)
 
 
-if (window.location.hash != "") {
-    $.getJSON(endpoint + "/" + hashh, function (data) {
-        data = data["result"];
+// if (window.location.hash != "") {
+//     $.getJSON(endpoint + "/" + hashh, function (data) {
+//         data = data["result"];
 
-        if (data != null) {
-            window.location.href = data;
-        }
+//         if (data != null) {
+//             window.location.href = data;
+//         }
 
-    });
-}
+//     });
+// }
 
 
 if (window.location.hash != "") {
