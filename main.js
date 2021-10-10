5ec181b3c0e8bdeaea4cf67faceca714f6fc1e1b
jess-dowd <jessalyndowd@gmail.com>
2021-10-10 17:19:19 +0700

    commit 4

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-var endpoint = "https://ef45989b94af8fb790427960f84d98ea.m.pipedream.net";
+// var endpoint = "https://ef45989b94af8fb790427960f84d98ea.m.pipedream.net";
 
-function geturl(){
+function geturl() {
     var url = document.getElementById("urlinput").value;
     var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
-    if(!protocol_ok){
-        newurl = "http://"+url;
+    if (!protocol_ok) {
+        newurl = "http://" + url;
         return newurl;
-        }else{
-            return url;
-        }
+    } else {
+        return url;
+    }
 }
 
 function getrandom() {
@@ -20,25 +20,44 @@ function getrandom() {
     return text;
 }
 
-function genhash(){
-    if (window.location.hash == ""){
+function genhash() {
+    if (window.location.hash == "") {
         window.location.hash = getrandom();
     }
 }
 
+// function send_request(url) {
+//     this.url = url;
+//     $.ajax({
+//         'url': endpoint + "/" + window.location.hash.substr(1),
+//         'type': 'POST',
+//         'data': JSON.stringify(this.url),
+//         'dataType': 'json',
+//         'contentType': 'application/json; charset=utf-8',
+//         'X-Master-Key': '$2b$10$awI5kl/9orunGmcpE2WBo.AjejhUK5Ml0nyHSKLm.tDWhwH30whUK'
+//     })
+// }
+
 function send_request(url) {
     this.url = url;
-    $.ajax({
-        'url': endpoint + "/" + window.location.hash.substr(1),
-        'type': 'POST',
-        'data': JSON.stringify(this.url),
-        'dataType': 'json',
-        'contentType': 'application/json; charset=utf-8',
-        'X-Master-Key': '$2b$10$awI5kl/9orunGmcpE2WBo.AjejhUK5Ml0nyHSKLm.tDWhwH30whUK'
-})
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+        if (req.readyState == XMLHttpRequest.DONE) {
+            console.log(req.responseText);
+        }
+    };
+
+    req.open("POST", "https://api.jsonbin.io/v3/b", true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader("X-Master-Key", "$2b$10$awI5kl/9orunGmcpE2WBo.AjejhUK5Ml0nyHSKLm.tDWhwH30whUK");
+    req.send(JSON.stringify(this.url));
 }
 
-function shorturl(){
+
+
+
+function shorturl() {
     var longurl = geturl();
     genhash();
     send_request(longurl);
@@ -46,13 +65,40 @@ function shorturl(){
 
 var hashh = window.location.hash.substr(1)
 
+// if (window.location.hash != "") {
+//     $.getJSON(endpoint + "/" + hashh, function (data) {
+//         data = data["result"];
+
+//         if (data != null) {
+//             window.location.href = data;
+//         }
+
+//     });
+// }
+
+
 if (window.location.hash != "") {
-    $.getJSON(endpoint + "/" + hashh, function (data) {
-        data = data["result"];
 
-        if (data != null) {
-            window.location.href = data;
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+        if (req.readyState == XMLHttpRequest.DONE) {
+            console.log(req.responseText);
         }
+    };
 
-    });
+    req.open("GET", "https://api.jsonbin.io/v3/b/<BIN_ID>/<BIN_VERSION | latest>", true);
+    req.setRequestHeader("X-Master-Key", "$2b$10$awI5kl/9orunGmcpE2WBo.AjejhUK5Ml0nyHSKLm.tDWhwH30whUK");
+    req.send();
+
+
+
+    data = data["result"];
+
+    if (data != null) {
+        window.location.href = data;
+    }
+
+
 }
+
